cbe7a1dda6271905bd3c6a0f64206cdd8bf5c5a1
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-14 10:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
-//your JS code here. If required.
-const getRandomTime = () => Math.floor(Math.random() * 3) + 1;
+// Get the table body element
+const tableBody = document.querySelector("#output");
 
-      const promises = [
-        new Promise((resolve) => setTimeout(() => resolve('Promise 1'), getRandomTime() * 1000)),
-        new Promise((resolve) => setTimeout(() => resolve('Promise 2'), getRandomTime() * 1000)),
-        new Promise((resolve) => setTimeout(() => resolve('Promise 3'), getRandomTime() * 1000)),
-      ];
+// Create an array with 3 Promises
+const promises = [
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise()
+];
 
-      Promise.all(promises)
-        .then((results) => {
-          const totalTime = results.reduce((acc, curr) => acc + getRandomTime(), 0);
-          const output = document.getElementById('output');
-          output.innerHTML = '';
-          results.forEach((result, i) => {
-            const row = document.createElement('tr');
-            const nameCell = document.createElement('td');
-            const timeCell = document.createElement('td');
-            nameCell.textContent = result;
-            timeCell.textContent = `${getRandomTime()}`;
-            row.appendChild(nameCell);
-            row.appendChild(timeCell);
-            output.appendChild(row);
-          });
-          const totalRow = document.createElement('tr');
-          const totalNameCell = document.createElement('td');
-          const totalTimeCell = document.createElement('td');
-          totalNameCell.textContent = 'Total';
-          totalTimeCell.textContent = `${totalTime.toFixed(3)}`;
-          totalRow.appendChild(totalNameCell);
-          totalRow.appendChild(totalTimeCell);
-          output.appendChild(totalRow);
-        })
-        .catch((error) => {
-          console.error(error);
-          const output = document.getElementById('output');
-          output.innerHTML = '<tr><td colspan="2">Error occurred while fetching data</td></tr>';
-        });
+// Add a loading row to the table
+const loadingRow = document.createElement("tr");
+const loadingCell = document.createElement("td");
+loadingCell.setAttribute("colspan", "2");
+loadingCell.textContent = "Loading...";
+loadingRow.appendChild(loadingCell);
+tableBody.appendChild(loadingRow);
+
+// Wait for all the Promises to resolve using Promise.all
+Promise.all(promises)
+  .then(results => {
+    // Remove the loading row from the table
+    tableBody.removeChild(loadingRow);
+
+    // Populate the table with the Promise results
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      const row = document.createElement("tr");
+      const nameCell = document.createElement("td");
+      nameCell.textContent = `Promise ${i + 1}`;
+      const timeCell = document.createElement("td");
+      timeCell.textContent = `${result.toFixed(3)}`;
+      row.appendChild(nameCell);
+      row.appendChild(timeCell);
+      tableBody.appendChild(row);
+    }
+
+    // Add a row for the total time taken
+    const totalTime = results.reduce((acc, curr) => acc + curr, 0);
+    const totalRow = document.createElement("tr");
+    const totalNameCell = document.createElement("td");
+    totalNameCell.textContent = "Total";
+    const totalTimeCell = document.createElement("td");
+    totalTimeCell.textContent = `${totalTime.toFixed(3)}`;
+    totalRow.appendChild(totalNameCell);
+    totalRow.appendChild(totalTimeCell);
+    tableBody.appendChild(totalRow);
+  });
+
+// Helper function to create a Promise that resolves after a random time between 1 and 3 seconds
+function createRandomPromise() {
+  const randomTime = Math.floor(Math.random() * 3000) + 1000;
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(randomTime / 1000);
+    }, randomTime);
+  });
+}
